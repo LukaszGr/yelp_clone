fc4fb18f87ae4b956ad41d89713d28d271f2801f
LukaszGr <lukasz.grychowski@gmail.com>
2016-07-15 10:52:10 +0100

    facebook sign in added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,13 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
+
+  def average_rating
+    if reviews.none?
+      'N/A'
+    else
+      reviews.average(:rating)
+    end
+  end
+
 end
